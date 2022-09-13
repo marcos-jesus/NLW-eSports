8a45c0c57e8ad1dc42b527423d08107d4f6b9426
marcos-jesus <marcosjesus.dev@gmail.com>
2022-09-13 19:43:34 -0300

    stage 2

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,41 @@
+import { View, Image, FlatList} from "react-native"
+
+import logoImg from "../../assets/logo-nlw-esports.png"
+
+import { GAMES } from "../../utils/games"
+
+import { Heading } from "../../components/Heading"
+import { GameCard } from "../../components/GameCard"
+
+import { styles } from "./styles"
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image 
+        source={logoImg}
+        style={styles.logo}
+      />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList 
+        contentContainerStyle={styles.contentList}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({item }) => (
+          <GameCard
+            data={item}
+          />
+        )}
+      />
+
+      
+    </View>
+  )
+}
